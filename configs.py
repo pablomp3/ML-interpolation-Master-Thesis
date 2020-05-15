362fd8fe7f76d9a49b2775af8d08082e55d69883
pablomp3 <44109383+pablomp3@users.noreply.github.com>
2020-05-15 23:26:34 +0800

    tf 1.14.0

---
+++ configs.py
@@ -0,0 +1,217 @@
+"""Configurations for MusicVAE models."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+'''from magenta.common import merge_hparams
+#from magenta.models.music_vae import data
+#from magenta.models.music_vae import data_hierarchical
+#from magenta.models.music_vae import lstm_models
+from magenta.models.music_vae.base_model import MusicVAE
+#from vae import MusicVAE
+import magenta.music as mm
+from tensorflow.contrib.training import HParams'''
+from vae import MusicVAE
+import lstm_models
+# from tf_utils import merge_hparams # function copied already so no need to import
+from hparam import HParams
+import data
+import magenta.music as mm
+
+class Config(collections.namedtuple(
+    'Config',
+    ['model', 'hparams', 'note_sequence_augmenter', 'data_converter',
+     'train_examples_path', 'eval_examples_path', 'tfds_name'])):
+
+  def values(self):
+    return self._asdict()
+
+Config.__new__.__defaults__ = (None,) * len(Config._fields)
+
+
+def update_config(config, update_dict):
+  config_dict = config.values()
+  config_dict.update(update_dict)
+  return Config(**config_dict)
+
+
+CONFIG_MAP = {}
+
+#------- copied from tf_utils ---------------------------------------------------
+def merge_hparams(hparams_1, hparams_2):
+  """Merge hyperparameters from two tf.contrib.training.HParams objects.
+  If the same key is present in both HParams objects, the value from `hparams_2`
+  will be used.
+  Args:
+    hparams_1: The first tf.contrib.training.HParams object to merge.
+    hparams_2: The second tf.contrib.training.HParams object to merge.
+  Returns:
+    A merged tf.contrib.training.HParams object with the hyperparameters from
+    both `hparams_1` and `hparams_2`.
+  """
+  hparams_map = hparams_1.values()
+  hparams_map.update(hparams_2.values())
+  return HParams(**hparams_map) # imported from hparam (tensorflow fork from a previous version that now is deprecated)
+#---------------------------------------------------------------------------------------
+
+# Melody
+CONFIG_MAP['cat-mel_2bar_small'] = Config(
+    model=MusicVAE(lstm_models.BidirectionalLstmEncoder(),
+                   lstm_models.CategoricalLstmDecoder()),
+    hparams=merge_hparams(
+        lstm_models.get_default_hparams(),
+        HParams(
+            batch_size=512,
+            max_seq_len=32,  # 2 bars w/ 16 steps per bar
+            z_size=256,
+            enc_rnn_size=[512],
+            dec_rnn_size=[256, 256],
+            free_bits=0,
+            max_beta=0.2,
+            beta_rate=0.99999,
+            sampling_schedule='inverse_sigmoid',
+            sampling_rate=1000,
+        )),
+    note_sequence_augmenter=data.NoteSequenceAugmenter(transpose_range=(-5, 5)),
+    data_converter=data.OneHotMelodyConverter(
+        valid_programs=data.MEL_PROGRAMS,
+        skip_polyphony=False,
+        max_bars=100,  # Truncate long melodies before slicing.
+        slice_bars=2,
+        steps_per_quarter=4),
+    train_examples_path=None,
+    eval_examples_path=None,
+)
+
+CONFIG_MAP['cat-mel_2bar_big'] = Config(
+    model=MusicVAE(lstm_models.BidirectionalLstmEncoder(),
+                   lstm_models.CategoricalLstmDecoder()),
+    hparams=merge_hparams(
+        lstm_models.get_default_hparams(),
+        HParams(
+            batch_size=512,
+            max_seq_len=32,  # 2 bars w/ 16 steps per bar
+            z_size=512,
+            enc_rnn_size=[2048],
+            dec_rnn_size=[2048, 2048, 2048],
+            free_bits=0,
+            max_beta=0.5,
+            beta_rate=0.99999,
+            sampling_schedule='inverse_sigmoid',
+            sampling_rate=1000,
+        )),
+    note_sequence_augmenter=data.NoteSequenceAugmenter(transpose_range=(-5, 5)),
+    data_converter=data.OneHotMelodyConverter(
+        valid_programs=data.MEL_PROGRAMS,
+        skip_polyphony=False,
+        max_bars=100,  # Truncate long melodies before slicing.
+        slice_bars=2,
+        steps_per_quarter=4),
+    train_examples_path=None,
+    eval_examples_path=None,
+)
+
+# Chord-Conditioned Melody
+CONFIG_MAP['cat-mel_2bar_med_chords'] = Config(
+    model=MusicVAE(lstm_models.BidirectionalLstmEncoder(),
+                   lstm_models.CategoricalLstmDecoder()),
+    hparams=merge_hparams(
+        lstm_models.get_default_hparams(),
+        HParams(
+            batch_size=512,
+            max_seq_len=32,  # 2 bars w/ 16 steps per bar
+            z_size=256,
+            enc_rnn_size=[1024],
+            dec_rnn_size=[512, 512, 512],
+        )),
+    note_sequence_augmenter=data.NoteSequenceAugmenter(
+        transpose_range=(-3, 3)),
+    data_converter=data.OneHotMelodyConverter(
+        max_bars=100,
+        slice_bars=2,
+        steps_per_quarter=4,
+        chord_encoding=mm.TriadChordOneHotEncoding()),
+    train_examples_path=None,
+    eval_examples_path=None,
+)
+
+# 16-bar Melody Models
+mel_16bar_converter = data.OneHotMelodyConverter(
+    skip_polyphony=False,
+    max_bars=100,  # Truncate long melodies before slicing.
+    slice_bars=16,
+    steps_per_quarter=4)
+
+CONFIG_MAP['flat-mel_16bar'] = Config(
+    model=MusicVAE(
+        lstm_models.BidirectionalLstmEncoder(),
+        lstm_models.CategoricalLstmDecoder()),
+    hparams=merge_hparams(
+        lstm_models.get_default_hparams(),
+        HParams(
+            batch_size=512,
+            max_seq_len=256,
+            z_size=512,
+            enc_rnn_size=[2048, 2048],
+            dec_rnn_size=[2048, 2048, 2048],
+            free_bits=256,
+            max_beta=0.2,
+        )),
+    note_sequence_augmenter=None,
+    data_converter=mel_16bar_converter,
+    train_examples_path=None,
+    eval_examples_path=None,
+)
+
+CONFIG_MAP['hierdec-mel_16bar'] = Config(
+    model=MusicVAE(
+        lstm_models.BidirectionalLstmEncoder(),
+        lstm_models.HierarchicalLstmDecoder(
+            lstm_models.CategoricalLstmDecoder(),
+            level_lengths=[16, 16],
+            disable_autoregression=True)),
+    hparams=merge_hparams(
+        lstm_models.get_default_hparams(),
+        HParams(
+            batch_size=512,
+            max_seq_len=256,
+            z_size=512,
+            enc_rnn_size=[2048, 2048],
+            dec_rnn_size=[1024, 1024],
+            free_bits=256,
+            max_beta=0.2,
+        )),
+    note_sequence_augmenter=None,
+    data_converter=mel_16bar_converter,
+    train_examples_path=None,
+    eval_examples_path=None,
+)
+
+CONFIG_MAP['hier-mel_16bar'] = Config(
+    model=MusicVAE(
+        lstm_models.HierarchicalLstmEncoder(
+            lstm_models.BidirectionalLstmEncoder, [16, 16]),
+        lstm_models.HierarchicalLstmDecoder(
+            lstm_models.CategoricalLstmDecoder(),
+            level_lengths=[16, 16],
+            disable_autoregression=True)),
+    hparams=merge_hparams(
+        lstm_models.get_default_hparams(),
+        HParams(
+            batch_size=512,
+            max_seq_len=256,
+            z_size=512,
+            enc_rnn_size=[1024],
+            dec_rnn_size=[1024, 1024],
+            free_bits=256,
+            max_beta=0.2,
+        )),
+    note_sequence_augmenter=None,
+    data_converter=mel_16bar_converter,
+    train_examples_path=None,
+    eval_examples_path=None,
+)
